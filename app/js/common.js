f1c6c624adc8657428ece75069b80c10dd47eba1
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-01-17 11:40:46 +0300

    sect4 done

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,19 @@ $(function() {
 		$(".main-menu").slideToggle();
 	});
 
-	$(".cards .card").equalHeights();
+	
+	$(".section_4 h2"). waypoint(function(){
+
+		$(".section_4 .card").each(function(index) {
+			var ths = $(this);
+			setInterval(function() {
+				ths.removeClass("card-off").addClass("card-on");
+			}, 150*index);
+		});
+
+		
+		
+	});
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
