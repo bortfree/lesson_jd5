8216830cf11d7cb9b28e62e9efadb21073490fb6
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-01-21 21:08:08 +0300

    sect8

--- app/js/common.js
+++ app/js/common.js
@@ -31,6 +31,21 @@ $(function() {
 	} );
 
 
+	$(".slider").owlCarousel({
+		items : 1,
+		nav : true,
+		navText : "",
+		loop : true,
+		autoplay : true,
+		autoplayHoverPause : true,
+		fluidSpeed : 600,
+		autoplaySpeed : 600,
+		navSpeed : 600,
+		dotsSpeed : 600,
+		dragEndSpeed : 600
+	});
+
+
 	
 
 
